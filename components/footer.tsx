42f0a6b2f1ad7c6d3dd57c3d55124d6642afe424
KJ <kjk@skyhopper.net>
2025-09-06 21:20:46 -0400

    margining

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 export default function Footer() {
     return (
-        <footer className="border-t-foreground border-t-[2px] w-full h-fit flex flex-col justify-center items-center py-16">
+        <footer className="border-t-foreground border-t-[2px] w-full h-fit flex flex-col justify-center items-center mt-8 py-16">
             <p>by Kj Kastning</p>
         </footer>
     );
